266f6b09c16b603ea703c0eadb1e20c641492aff
bammite_prince <bammite_prince>
2024-10-18 16:14:28 +0000

    2end

--- Third_ren/Javascript/general.js
+++ Third_ren/Javascript/general.js
@@ -11,71 +11,71 @@ const navslide =() =>{
 
 navslide();
 
-var swiper;
+// var swiper;
 
-function initSwiper() {
-    if (window.innerWidth < 768) {
-        swiper = new Swiper(".slide-contenair", {
-            slidesPerView: 1,
-            spaceBetween: 30,
-            freeMode: true,
-            centerslide: true,
-            fade: true,
-            grabCursor: true,
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: true,
-                dynamicBullets: true,
-            },
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
-            },
-        });
-    } 
-    else{
-        if (window.innerWidth < 1000){
-        swiper = new Swiper(".slide-contenair", {
-            slidesPerView: 2,
-            spaceBetween: 30,
-            freeMode: true,
-            centeredSlides: true,
-            fade: true,
-            grabCursor: true,
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: true,
-                dynamicBullets: true,
-            },
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
-            },
-        });
-    }
-    else{
-        swiper = new Swiper(".slide-contenair", {
-            slidesPerView: 3, 
-            spaceBetween: 30,
-            freeMode: true,
-            centeredSlides: true,
-            fade: true,
-            grabCursor: true,
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: true,
-                dynamicBullets: true,
-            },
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
-            },
-        });
-    }
-    }
-}
-initSwiper();
-window.addEventListener('resize', function() {
-    swiper.destroy();
-    initSwiper();
-});
+// function initSwiper() {
+//     if (window.innerWidth < 768) {
+//         swiper = new Swiper(".slide-contenair", {
+//             slidesPerView: 1,
+//             spaceBetween: 30,
+//             freeMode: true,
+//             centerslide: true,
+//             fade: true,
+//             grabCursor: true,
+//             pagination: {
+//                 el: ".swiper-pagination",
+//                 clickable: true,
+//                 dynamicBullets: true,
+//             },
+//             navigation: {
+//                 nextEl: ".swiper-button-next",
+//                 prevEl: ".swiper-button-prev",
+//             },
+//         });
+//     } 
+//     else{
+//         if (window.innerWidth < 1000){
+//         swiper = new Swiper(".slide-contenair", {
+//             slidesPerView: 2,
+//             spaceBetween: 30,
+//             freeMode: true,
+//             centeredSlides: true,
+//             fade: true,
+//             grabCursor: true,
+//             pagination: {
+//                 el: ".swiper-pagination",
+//                 clickable: true,
+//                 dynamicBullets: true,
+//             },
+//             navigation: {
+//                 nextEl: ".swiper-button-next",
+//                 prevEl: ".swiper-button-prev",
+//             },
+//         });
+//     }
+//     else{
+//         swiper = new Swiper(".slide-contenair", {
+//             slidesPerView: 3, 
+//             spaceBetween: 30,
+//             freeMode: true,
+//             centeredSlides: true,
+//             fade: true,
+//             grabCursor: true,
+//             pagination: {
+//                 el: ".swiper-pagination",
+//                 clickable: true,
+//                 dynamicBullets: true,
+//             },
+//             navigation: {
+//                 nextEl: ".swiper-button-next",
+//                 prevEl: ".swiper-button-prev",
+//             },
+//         });
+//     }
+//     }
+// }
+// initSwiper();
+// window.addEventListener('resize', function() {
+//     swiper.destroy();
+//     initSwiper();
+// });
